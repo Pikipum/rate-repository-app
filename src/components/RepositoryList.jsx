df2d0e3191fe79994893eac25b8d909b74453d04
Markus Tervonen <markus.tervonen@hotmail.com>
2025-11-07 14:27:05 +0200

    teht 10.23

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,40 +2,78 @@ import { FlatList, View, StyleSheet, Pressable } from "react-native";
 import RepositoryItem from "./RepositoryItem";
 import useRepositories from "../hooks/useRepositories";
 import { useNavigate } from "react-router-native";
+import { Picker } from "@react-native-picker/picker";
+import { useState } from "react";
 
 const styles = StyleSheet.create({
   separator: {
     height: 10,
   },
+  pickerContainer: {
+    backgroundColor: "#e1e4e8",
+    padding: 15,
+  },
+  picker: {
+    backgroundColor: "white",
+    borderRadius: 4,
+    marginHorizontal: 10,
+  },
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-export const RepositoryListContainer = ({ navigate, repositories }) => {
+export const RepositoryListContainer = ({
+  setSelectedFilter,
+  selectedFilter,
+  navigate,
+  repositories,
+}) => {
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
     : [];
 
   return (
-    <FlatList
-      data={repositoryNodes}
-      ItemSeparatorComponent={ItemSeparator}
-      renderItem={({ item }) => (
-        <Pressable onPress={() => navigate(`/${item.id}`)}>
-          <RepositoryItem repository={item} />
-        </Pressable>
-      )}
-      keyExtractor={(item) => item.id}
-    />
+    <>
+      <View style={styles.pickerContainer}>
+        <Picker
+          style={styles.picker}
+          selectedValue={selectedFilter}
+          onValueChange={(itemValue) => setSelectedFilter(itemValue)}
+        >
+          <Picker.Item label="Latest repositories" value="LATEST" />
+          <Picker.Item
+            label="Highest rated repositories"
+            value="HIGHEST_RATED"
+          />
+          <Picker.Item label="Lowest rated repositories" value="LOWEST_RATED" />
+        </Picker>
+      </View>
+      <FlatList
+        data={repositoryNodes}
+        ItemSeparatorComponent={ItemSeparator}
+        renderItem={({ item }) => (
+          <Pressable onPress={() => navigate(`/${item.id}`)}>
+            <RepositoryItem repository={item} />
+          </Pressable>
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    </>
   );
 };
 
 const RepositoryList = () => {
+  const [selectedFilter, setSelectedFilter] = useState("LATEST");
   const navigate = useNavigate();
-  const { repositories } = useRepositories();
+  const { repositories } = useRepositories(selectedFilter);
 
   return (
-    <RepositoryListContainer navigate={navigate} repositories={repositories} />
+    <RepositoryListContainer
+      selectedFilter={selectedFilter}
+      setSelectedFilter={setSelectedFilter}
+      navigate={navigate}
+      repositories={repositories}
+    />
   );
 };
 
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,9 +1,23 @@
 import { useQuery } from "@apollo/client";
 import { GET_REPOSITORIES } from "../graphql/queries";
 
-const useRepositories = () => {
+const useRepositories = (selectedFilter) => {
+  let variables = {};
+
+  switch (selectedFilter) {
+    case "HIGHEST_RATED":
+      variables = { orderBy: "RATING_AVERAGE", orderDirection: "DESC" };
+      break;
+    case "LOWEST_RATED":
+      variables = { orderBy: "RATING_AVERAGE", orderDirection: "ASC" };
+      break;
+    default:
+      variables = { orderBy: "CREATED_AT", orderDirection: "DESC" };
+  }
+
   const { data, error, loading, refetch } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: "cache-and-network",
+    variables,
   });
 
   const repositories = data?.repositories ?? null;
